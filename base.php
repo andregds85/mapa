8e150fe8f67ebe2ff41162c5f53658996689bb73
André Gomes da Silva <andregds85@gmail.com>
2022-05-06 19:03:00 -0300

    teste
    teste

---
+++ base.php
@@ -0,0 +1,28 @@
+
+<script>
+
+
+
+
+Antônio Carlos,
+map.setView([-27.5179, -48.7685], 5);
+
+
+
+</scrpit>
+
+
+
+
+
+
+
+
+ Balneário Gaivota, Biguaçu, Canelinha, Entre Rios, Faxinal dos Guedes, Floranópolis,
+  Garopaba, Governador Celso Ramos, Imaruí, Jacinto Machado, Leoberto Leal, Major Gercino,
+   Meleiro, Nova Trento, Palhoça, Paulo Lopes, Passo de Torres, Praia Grande, Rancho Queimado, 
+   Santo Amaro da Imperatriz, São Bonifácio, São João Batista, Santa Rosa do Sul, São João do Sul, 
+   Sombrio, Timbé do Sul, Turvo, Xanxerê e Xaxim.
+
+
+
