93ff6e124848f41a336e924aaa9b5a82cfc4f6c4
André Gomes da Silva <andregds85@gmail.com>
2022-05-09 19:25:47 -0300

    teste
    teste

--- base.php
+++ base.php
@@ -3,9 +3,10 @@
 
 
 
+/* Governador Celso */ 
+map.setView([-28.3442, -48.8160], 5);
 
-Antônio Carlos,
-map.setView([-27.5179, -48.7685], 5);
+
 
 
 
@@ -17,9 +18,7 @@ map.setView([-27.5179, -48.7685], 5);
 
 
 
-
- Balneário Gaivota, Biguaçu, Canelinha, Entre Rios, Faxinal dos Guedes, Floranópolis,
-  Garopaba, Governador Celso Ramos, Imaruí, Jacinto Machado, Leoberto Leal, Major Gercino,
+ Imaruí, Jacinto Machado, Leoberto Leal, Major Gercino,
    Meleiro, Nova Trento, Palhoça, Paulo Lopes, Passo de Torres, Praia Grande, Rancho Queimado, 
    Santo Amaro da Imperatriz, São Bonifácio, São João Batista, Santa Rosa do Sul, São João do Sul, 
    Sombrio, Timbé do Sul, Turvo, Xanxerê e Xaxim.
